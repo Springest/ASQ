326ef08154981c7284610d97b93852c9c1e036cd
Mark Mulder <markmulder@gmail.com>
2017-04-25 11:45:22 +0200

    Set max_connections for sequel

--- docker_runner.rb
+++ docker_runner.rb
@@ -17,6 +17,7 @@ File.open("/app/config.yml", "w") do |config|
   config.puts "  user: #{get_env_or_fail("DB_USERNAME")}"
   config.puts "  password: #{get_env_or_fail("DB_PASSWORD")}"
   config.puts "  database: #{get_env_or_fail("DB_NAME")}"
+  config.puts "  max_connections: 20"
 
   config.puts "read_databases:"
 
